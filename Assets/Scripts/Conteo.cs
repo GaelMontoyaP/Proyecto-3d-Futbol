0cff8002e9303f08dbadae9ef27eb02bcc61a99c
GaelMontoyaP <gael@eliumontoya.com>
2026-08-06 18:00:17 -0600

    Script para conteo de goles

---
+++ Assets/Scripts/Conteo.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Conteo : MonoBehaviour
+{
+    [Header("Marcador UI")]
+    [Tooltip("Arrastra aquí tu texto de TextMeshPro de la jerarquía")]
+    [SerializeField] private TextMeshProUGUI textoGoles; 
+
+    // El contador numérico secreto
+    private int goles = 0; 
+
+    // Esta función se activa SOLA cuando el balón atraviesa un "Trigger"
+    private void OnTriggerEnter(Collider otro)
+    {
+        // Preguntamos si lo que tocamos tiene el Tag "Gol"
+        if (otro.gameObject.CompareTag("Pared"))
+        {
+            // Sumamos 1 a nuestro contador
+            goles++; 
+
+            // Actualizamos el texto en la pantalla
+            textoGoles.text = "Goles: " + goles; 
+
+            Debug.Log("¡GOOOOOL!");
+        }
+    }
+}
